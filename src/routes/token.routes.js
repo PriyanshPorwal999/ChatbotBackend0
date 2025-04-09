dcc8b442b4299c39f9b6e5a9dc9b37d98abce934
Priyansh Porwal <priyanshporwal2004@gmail.com>
2025-04-09 15:07:34 +0530

    Update token.routes.js

--- src/routes/token.routes.js
+++ src/routes/token.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { handleTokenRequest } = require('../controllers/Token.controller.js');
 
 // Use the same route for both generate and validate
-router.post('/token', handleTokenRequest);
+router.get('/token', handleTokenRequest);
 
 router.get('/test', (req, res) => {
     res.json({ msg: "GET /api/test working!" });
